d6c9b1ddec9cbd0187b46f0375b72bc33402476c
iFran2019 <fran1234532@hotmail.com>
2023-11-24 22:46:29 +0100

    Fixed AdminFile.java

--- src/main/java/net/kappa/boxpvp/files/list/messages/AdminFile.java
+++ src/main/java/net/kappa/boxpvp/files/list/messages/AdminFile.java
@@ -38,9 +38,9 @@ public class AdminFile {
         admin_general_insufficientArgs = ColorUtil.translate(Objects.requireNonNull(content.getString("admin.general.insufficientArgs")));
 
         // Commands - Claim
-        admin_command_claim_create = ColorUtil.translate(Objects.requireNonNull(content.getString("admin.command.claim.create")));
-        admin_command_claim_unplacedPositions = ColorUtil.translate(Objects.requireNonNull(content.getString("admin.command.claim.unplacedPositions")));
-        admin_command_claim_cancel = ColorUtil.translate(Objects.requireNonNull(content.getString("admin.command.claim.cancel")));
+        admin_command_claim_create = ColorUtil.translate(Objects.requireNonNull(content.getString("admin.commands.claim.create")));
+        admin_command_claim_unplacedPositions = ColorUtil.translate(Objects.requireNonNull(content.getString("admin.commands.claim.unplacedPositions")));
+        admin_command_claim_cancel = ColorUtil.translate(Objects.requireNonNull(content.getString("admin.commands.claim.cancel")));
 
 
     }
